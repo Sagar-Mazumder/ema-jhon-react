7779f9bed6db6627b8303f4353140364699249d1
sagar <sagarmazumder50.com>
2021-10-01 20:45:19 +0600

    loaded and change

---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Order = () => {
+      return (
+            <div>
+                  <h1>Hi i am sagar mazumder</h1>
+            </div>
+      );
+};
+
+export default Order;
